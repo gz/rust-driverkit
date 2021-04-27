985f0033acffcdb66f2a824ad4f18bac3ad70815
Reto Achermann <achreto@gmail.com>
2021-04-27 16:18:08 -0700

    devq: tweaking the trait
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/devq.rs
+++ src/devq.rs
@@ -7,22 +7,24 @@ use crate::iomem::IOBufChain;
 custom_error! {pub DevQueueError
     BufferInvalid = "one of the supplied buffers was invalid",
     OutOfMemory = "the operation caused an out-of-memory condition",
-    QueueFailure = "Unknown queue failer",
+    QueueFull = "the queue was full. Can't enqueue more buffers.",
+    QueueEmpty = "the queue was empty. Nothing to deuqueue.",
+    QueueFailure = "Unknown queue failure",
 }
 
 /// A device queue interface supporting enqueue/dequeueu
 pub trait DevQueue {
-    /// Enqueues one or multiple IOBufChains into the queue that implements this
+    /// Enqueues an IOBufChain into the queue that implements this
     /// trait. This updates the descriptors of the queue accordingly, but the
     /// buffers may not yet be made available for the device, and a flush() is
     /// required afterwards.
     ///
-    /// The function returns buffer chains that have not been equeued (e.g., due
-    /// to limited available space on the queue).
+    /// The function returns the buffer chain if there was not enough space to enqueue all
+    /// buffers in this chain. (e.g., due to limited available space on the queue).
     ///
     /// # Arguments
     /// - bufs: a vector of buffers chains to be enqueued on the card
-    fn enqueue(&mut self, bufs: IOBufChain) -> Result<IOBufChain, DevQueueError>;
+    fn enqueue(&mut self, bufs: IOBufChain) -> Result<(), IOBufChain>;
 
     /**
      * notifies the device that there have been new descriptors added to the queue
@@ -40,12 +42,10 @@ pub trait DevQueue {
     fn can_enqueue(&self, exact: bool) -> Result<usize, DevQueueError>;
 
     /**
-     * dequeues up to a number of `cnt` buffers from the queue. The buffers are returned
+     * dequeues a previously enqueued buffer chain from the queue. The buffers are returned
      * as the chains they were enqueued in.
-     *
-     *  - cnt: the maximum amount of buffers to process
      */
-    fn dequeue(&mut self, cnt: usize) -> Result<VecDeque<IOBufChain>, DevQueueError>;
+    fn dequeue(&mut self) -> Result<IOBufChain, DevQueueError>;
 
     /**
      * Checks if there are buffers ready to be dequeued and returns the count of processed
